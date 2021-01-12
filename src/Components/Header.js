6366b92acf71416997a95d85b184c2a1ad8025f9
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-01-12 15:19:53 -0600

    add grid with badges to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { AppBar, Toolbar } from '@material-ui/core'
-
+import { AppBar, Toolbar, Grid, InputBase, IconButton, Badge } from '@material-ui/core'
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone'; 
 function Header() {
     return (
       <AppBar position='static'> 
-      <Toolbar>
-          
-      </Toolbar>
+        <Toolbar>
+            <Grid container>
+                <Grid item sm={4} style={{border: '1px solid #fff'}}>
+                    <InputBase /> 
+                </Grid>
+                <Grid item sm={8} style={{border: '1px solid #000'}}>
+                    <IconButton>
+                        <Badge badgeContent={4} color='secondary'>
+<NotificationsNoneIcon />
+                        </Badge>
+                    </IconButton>
+                </Grid>
+            </Grid>
+        </Toolbar>
       </AppBar>
     )
 }
