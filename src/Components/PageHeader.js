1c69acf1d9c8dd16817f1acbfe4b8da7aea3106f
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-01-12 17:04:53 -0600

    create custom styling for pageHeader component

--- src/Components/PageHeader.js
+++ src/Components/PageHeader.js
@@ -1,16 +1,39 @@
 import React from 'react'
-import { Paper, Card, Typography } from '@material-ui/core'
+import { Paper, Card, Typography, makeStyles } from '@material-ui/core'
+import { FullscreenExit } from '@material-ui/icons';
 
+
+const useStyles = makeStyles(theme =>({
+    root:{
+        backgroundColor: '#fdfdff'
+    }, 
+    pageHeader: {
+        padding: theme.spacing(4),
+        display: 'flex',
+        marginBottom: theme.spacing(3) 
+    }, 
+    pageIcon:{
+        display:'inline-block',
+        padding:theme.spacing(2), 
+        color: '#3c44b1'
+    }, 
+    pageTitle:{
+        paddingLeft: theme.spacing(4), 
+        '& .MuiTypography-subtitle2':{
+            opacity:'0.6'
+        }
+    }
+}))
 function PageHeader(props) {
-
+    const classes = useStyles(); 
     const {title, subtitle, icon} = props
     return (
-        <Paper elevation={0} square>
-            <div>
-                <Card>
+        <Paper elevation={0} square className={classes.root}>
+            <div className={classes.pageHeader}>
+                <Card className={classes.pageIcon}>
                     {icon}
                 </Card>
-                <div>
+                <div className={classes.pageTitle}>
                     <Typography 
                     variant="h6"
                     component='div'
